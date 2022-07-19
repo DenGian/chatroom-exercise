cb802c7f9e07d97c12d2793434930b3530cbca22
DenGian <mondelaers.ian@gmail.com>
2022-07-19 10:18:14 +0200

    server is live

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const http = require('http');
+
+const app = express(); //To define our application
+const clientPath = `${__dirname}/../client`; //To give the path to our client
+app.use(express.static(clientPath)); //To use express to host the client
+const server = http.createServer(app); //To use http to serve the app that express provides
+
+server.listen(8080, () =>{
+    console.log("server running on "+8080); // node server -> logs "server running on " in terminal
+});
