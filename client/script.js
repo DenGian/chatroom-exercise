e031f7747d37c191ee82fffa1dead54990a13f17
DenGian <mondelaers.ian@gmail.com>
2022-07-19 14:34:11 +0200

    add sendToSelf functionality

--- client/script.js
+++ client/script.js
@@ -1,11 +1,21 @@
 // variables
 let socket = io.connect(); //to define your socket
 
+// call to display message
+socket.on('displayMessage', (message) => {
+    target.innerHTML += '<br>'+message;
+});
+
+// send to all functionality
 document.getElementById('all').addEventListener('click' , clickAll)
 function clickAll(){
     let message= document.getElementById('message').value;
     socket.emit('sendToAll', (message));
 }
-socket.on('displayMessage', (message) => {
-    target.innerHTML += '<br>'+message;
-});
+
+// send to self functionality
+document.getElementById('lonely').addEventListener('click', clickEcho)
+function clickEcho(){
+    let message= document.getElementById('message').value;
+    socket.emit('sendToSelf', (message));
+}
